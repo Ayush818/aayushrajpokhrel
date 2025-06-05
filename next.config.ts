aada4f89b00307bb586a4b25b8754f44ef283c2f
Ayush818 <aayushraj.pokhrel@gmail.com>
2025-06-05 22:45:11 +0545

    Add .nojekyll to fix CSS/JS loading

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/aayushrajpokhrel',
-  assetPrefix: '/aayushrajpokhrel/',
   trailingSlash: true,
 
 
