9eea9bd137720fd4e7111a39e912ed6d60a6ca02
Ayush818 <aayushraj.pokhrel@gmail.com>
2025-06-05 21:39:52 +0545

    added output:export in config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   /* config options here */
+  basePath: '/aayushrajpokhrel',
+
 };
 
 export default nextConfig;
