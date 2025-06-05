0930d06e58fe644014b6cee725efd889811822ff
Ayush818 <aayushraj.pokhrel@gmail.com>
2025-06-05 22:14:57 +0545

    fix trailingslash issue

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   /* config options here */
-  basePath: '/aayushrajpokhrel',
   trailingSlash: true,         // ensures correct linking of CSS/JS
 
 
