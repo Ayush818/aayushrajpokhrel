595adf9052189db5916dfaf4984ef79b00a14483
Aayush Raj Pokhrel <58337078+Ayush818@users.noreply.github.com>
2025-06-06 12:39:34 +0545

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/aayushrajpokhrel',
-  assetPrefix: '/aayushrajpokhrel/',
+  basePath: '',
+  assetPrefix: '/',
   trailingSlash: true,
 
 
